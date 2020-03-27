a19bdb47ff891fa3aabf4dcc1751ff0ce839c3a8
elijahmeyer <meyer.263@wright.edu>
2020-03-26 20:49:13 -0400

    added -y to install commands to get rid of annoying y/n prompt

--- install_docker.sh
+++ install_docker.sh
@@ -4,7 +4,7 @@ sudo apt-get remove docker docker-engine docker.io containerd runc
 
 sudo apt-get update
 
-sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
@@ -12,6 +12,6 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 
 sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 echo "Now run: sudo docker run hello-world to test that it worked"
